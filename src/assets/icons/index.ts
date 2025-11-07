d728b7a26b3116f0c717e557769d3f2536fd0228
Tomek <tomasz.szczygiel@4soft.co>
2025-11-07 11:41:01 +0100

    feature: added edit delete icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,9 @@
-export enum Icons {
-  Eye = "/icons/eye.svg",
-}
+import Eye from "./eye.svg?url";
+import Delete from "./delete.svg?url";
+import Edit from "./edit.svg?url";
+
+export const Icons = {
+  Eye,
+  Delete,
+  Edit,
+} as const;
--- src/components/ui/BoardListTile.tsx
+++ src/components/ui/BoardListTile.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import type { BoardSummaryDTO } from "@/types";
+import { Icons } from "@/assets/icons";
 
 interface IBoardListTileProps {
     board: BoardSummaryDTO;
@@ -20,27 +21,41 @@ export const BoardListTile: FC<IBoardListTileProps> = ({ board }) => {
     font-bold
     capitalize
     cursor-pointer 
+    w-full
+    justify-between
     flex">
-        <div className="
-        w-[40px] 
-        h-[40px] 
-        rounded-[20px] 
-        border-2 border-blue-500 
-        bg-white 
-        text-[var(--color-primary)]
-        flex 
-        justify-center 
-        items-center 
-        mr-2" >
-            {board.title.slice(0, 1)}        </div>
-        <div className="flex flex-col">
+        <div className="flex">
             <div className="
-            relative 
-            ">
-            {board.title}
+            w-[40px] 
+            h-[40px] 
+            rounded-[20px] 
+            border-2 border-blue-500 
+            bg-white 
+            text-[var(--color-primary)]
+            flex 
+            justify-center 
+            items-center 
+            mr-2" >
+                {board.title.slice(0, 1)}        
             </div>
-            <div>Level: {board.level}</div>
+            <div className="flex flex-col">
+                <div className="relative">
+                    {board.title}
+                </div>
+                <div className="text-sm text-gray-500">Level: {board.level}</div>
+            </div>
         </div>
-
+        <div className="flex items-center gap-2">
+            <img
+                src={Icons.Edit}
+                alt="Edytuj"
+                className="cursor-pointer select-none w-[20px] h-[20px] relative z-10"
+            />
+            <img
+                src={Icons.Delete}
+                alt="Usuń"
+                className="cursor-pointer select-none w-[20px] h-[20px] relative z-10"
+            />
+        </div>
     </a>)
 }
