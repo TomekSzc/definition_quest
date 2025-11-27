933df7948876b0461d6bd7499157db858a519e52
Tomek <tomasz.szczygiel@4soft.co>
2025-11-27 16:29:58 +0100

    feat: added docker filer

--- src/components/HOC/Providers.tsx
+++ src/components/HOC/Providers.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import { useMemo } from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, getPersistor } from "@/store";
 import Toast from "../ui/Toast";
 import type { FC } from "react";
